5233f473c1125280079caecc22ecce00edd73483
pasco-pasco <pchenier@gmail.com>
2025-12-09 21:34:03 -0500

    Fix build error: remove unused buttonVariants import

--- src/components/ui/button-field.tsx
+++ src/components/ui/button-field.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { Button, buttonVariants, type ButtonProps } from "@/components/ui/button"
+import { Button, type ButtonProps } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
 
 export interface ButtonFieldProps extends ButtonProps {
